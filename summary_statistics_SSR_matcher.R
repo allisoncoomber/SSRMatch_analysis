76622229a86b73321ff5672305be3c049c6530bf
Dave Fol <alc266@cornell.edu>
2020-09-11 16:01:56 -0400

    Added functions to make summary statistics for all the classes.

--- summary_statistics_SSR_matcher.R
+++ summary_statistics_SSR_matcher.R
@@ -1,7 +1,9 @@
 library(dplyr)
+library(caret)
 
 #Create a list
 confusionMatrices <- list()
+genotypePredictionResults <- list()
 
 # Read in the confusion matrices into a list of dataframes
 for(i in 1:5) {
@@ -13,3 +15,18 @@ bind_rows(confusionMatrices, .id = "column_label") %>%
   group_by(X) %>%
   summarise(across(c(BR_1.:US_17.), sum)) %>%
   write.csv(paste("/Users/Allison/git/SSRmatch_analysis/K1_summary_confusion_matrix.csv", sep=","), fileEncoding="UTF-8")
+
+# Read in the prediction results into a list of dataframes
+for(i in 1:5) {
+  genotypePredictionResults[[i]] <- read.table(paste("/Users/Allison/git/SSRmatch_analysis/K1_sample", i, "_results.txt", sep=""), header=TRUE, encoding="UTF-8", sep = "\t")
+}
+
+# Bind all of the results into one big table
+genotypePredictionTable <- bind_rows(genotypePredictionResults)
+
+# Coerce the columns to factors and give them new names
+genotypePredictionTable <- rename(genotypePredictionTable, c("predictedGenotype"="MatchingGenotype.", "actualGenotype"="Genotype."))
+genotypePredictionTable$predictedGenotype <- as.factor(genotypePredictionTable$predictedGenotype)
+genotypePredictionTable$actualGenotype <- as.factor(genotypePredictionTable$actualGenotype)
+
+summaryStatistics <- confusionMatrix(genotypePredictionTable$predictedGenotype, genotypePredictionTable$actualGenotype)
